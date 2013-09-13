91dc27f33360381a3529cf0d6e6507c80e022a2b
Matthew King <mking@crownchimp.com>
2013-09-13 14:59:33 -0700

    Commit 1
    Progress on the homepage animation

--- bottom-nav-rev1/assets/js/application.js
+++ bottom-nav-rev1/assets/js/application.js
@@ -25,5 +25,16 @@ $(document).ready(function() {
 		classie.remove(bottomLinks, 'in');
 	});
 
+	$('.homepage-circles').on('click', function() {
+		classie.add(this, 'active-circle');
+		classie.add($('#camera')[0], 'camera-circle-animation');
+		classie.add($('#text')[0], 'text-circle-animation');
+		classie.add($('#video')[0], 'video-circle-animation');
+		var m = setTimeout(function() { 
+			classie.add($('#modal-bg-parent')[0], 'in') 
+			classie.add($('#featured-modal')[0], 'in') 
+		}, 1000);
+	});
+
 
 });
