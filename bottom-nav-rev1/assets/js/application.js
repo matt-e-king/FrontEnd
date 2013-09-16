0b5e8ff16d99d78ee50646ce6ecabcdcaedb1b50
Matthew King <mking@crownchimp.com>
2013-09-16 13:33:41 -0700

    Commit 2
    Conitnued progress on animating the homepage circles

--- bottom-nav-rev1/assets/js/application.js
+++ bottom-nav-rev1/assets/js/application.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
 
-	/*var pageHeight = $(window).height();
-	var adjustedHeight = pageHeight * .70;
-
-	$('.stories-parent').height(adjustedHeight);*/
+	$(".nau-video").fitVids();
 
 	$('#stories-slider').carousel({
   		interval: false
@@ -23,17 +20,6 @@ $(document).ready(function() {
 
 	$('.stories-collapse').on('hide.bs.collapse', function() {
 		classie.remove(bottomLinks, 'in');
-	});
-
-	$('.homepage-circles').on('click', function() {
-		classie.add(this, 'active-circle');
-		classie.add($('#camera')[0], 'camera-circle-animation');
-		classie.add($('#text')[0], 'text-circle-animation');
-		classie.add($('#video')[0], 'video-circle-animation');
-		var m = setTimeout(function() { 
-			classie.add($('#modal-bg-parent')[0], 'in') 
-			classie.add($('#featured-modal')[0], 'in') 
-		}, 1000);
 	});
 
 
